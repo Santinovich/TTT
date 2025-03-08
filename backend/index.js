64a2ee83367fd7f9e82d871b58fa44e6c085abee
SO <santino.villar.20@gmail.com>
2025-03-08 00:51:53 -0300

    server express

--- backend/index.js
+++ backend/index.js
@@ -2,4 +2,14 @@ const sqlite = require("sqlite3")
 
 const db = new sqlite.Database("TTT.db");
 
-db.all("select * from barrio",(error,rows)=>{console.log(rows)})
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+res.send('Successful response.');
+});
+
+app.listen(port, () => {
+console.log(`Example app is listening on port ${port}.`);
+});
